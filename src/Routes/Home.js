719025275caed5a0e834d2a8b9b08d5a4ad01c10
Anthony Lee <anthonyleehj@gmail.com>
2022-12-13 15:27:57 +0800

    Added navigation styling

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='home-desc'></div>
+      <div className='home-img'></div>
+    </div>
   )
 }
 

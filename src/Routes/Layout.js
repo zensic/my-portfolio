6868f53bafa8e0ba6cfc31055515128af494c5e8
Anthony Lee <anthonyleehj@gmail.com>
2022-12-13 14:36:35 +0800

    Added navigation bar

--- src/Routes/Layout.js
+++ src/Routes/Layout.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Outlet } from 'react-router'
+import { Link } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <nav>
+        <Link to={``}>Home</Link>
+        <Link to={``}>About</Link>
+        <Link to={``}>Projects</Link>
+      </nav>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
